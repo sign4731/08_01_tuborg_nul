e84ce13935fa1591eace6469ef3a108c7185fb6d
sign4731 <sign4731@stud.kea.dk>
2020-10-10 16:55:10 +0200

    mobil version

---
+++ js/script.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", sidenVises)
+
+function sidenVises() {
+    //popupvinduer
+    document.querySelector(".info_klikmig_menu").addEventListener("click", () => popup_info.style.display = "block");
+    document.querySelector(".info_klikmig").addEventListener("click", () => popup_info.style.display = "block");
+    document.querySelector("#luk_info").addEventListener("click", () => popup_info.style.display = "none");
+
+    document.querySelector(".quiz_klikmig_menu").addEventListener("click", () => popup_quiz.style.display = "block");
+    document.querySelector(".quiz_klikmig").addEventListener("click", () => popup_quiz.style.display = "block");
+    document.querySelector("#luk_quiz").addEventListener("click", () => popup_quiz.style.display = "none");
+
+    document.querySelector(".infografik_klikmig_menu").addEventListener("click", () => popup_infografik.style.display = "block");
+    document.querySelector(".infografik_klikmig").addEventListener("click", () => popup_infografik.style.display = "block");
+    document.querySelector("#luk_infografik").addEventListener("click", () => popup_infografik.style.display = "none");
+
+    //menuknap
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+}
+
+
+//basic burgermenu
+function toggleMenu() {
+    document.querySelector("#menu").classList.toggle("hidden");
+
+    let erSkjult = document.querySelector("#menu").classList.contains("hidden");
+
+    if (erSkjult == true) {
+        document.querySelector("#menuknap").textContent = "☰";
+    } else {
+        document.querySelector("#menuknap").textContent = "x";
+    }
+
+}
